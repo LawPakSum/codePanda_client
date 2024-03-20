aef4b3d5c3c1f3c68c725b80a033816af33aba77
TheRobert0728 <69301840+TheRobert0728@users.noreply.github.com>
2024-03-21 00:38:08 +0800

    Push Routes

---
+++ src/Routes/Router.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
+
+function Router() {
+  return (
+    <Router>
+        <Routes>
+            {/* to be continue
+            <Route path='/'></Route>
+            <Route path='/'></Route>
+            <Route path='/'></Route>
+            <Route path='/'></Route> */}
+        </Routes>
+    </Router>
+  )
+}
+
+export default Router
